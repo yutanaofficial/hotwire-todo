599e5939dc1884c8de8c4b0bb7046d7fa2f576c0
Ruuf <ruuf@odds.team>
2024-08-19 23:03:44 +0700

    Test model

--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class TodoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "should not save todo without name" do
+    todo = Todo.new
+    assert_not todo.save, "Save todo without name"
+  end
 end
