dd4bfae64b3e74cac60ddf0790143785dd9eb8dc
RuuF <roof@odds.team>
2024-08-20 11:41:04 +0700

    Tab for complete and incomplete

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,7 +3,7 @@ class TodosController < ApplicationController
 
   # GET /todos or /todos.json
   def index
-    @todos = Todo.all
+    @todos = Todo.where(status: params[:status].presence || 'incomplete')  
   end
 
   # GET /todos/1 or /todos/1.json
