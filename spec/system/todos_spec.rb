23c8a25b7318492b4d5e390f1a6aafffa8af0d8c
Ruuf <ruuf@odds.team>
2024-09-02 09:05:59 +0700

    Add delete and update todo

--- spec/system/todos_spec.rb
+++ spec/system/todos_spec.rb
@@ -20,4 +20,26 @@ RSpec.describe "Todos", type: :system do
     expect(Todo.count).to eq(1)
     expect(Todo.last.name).to eq("Buy Banana")
   end  
+
+  scenario 'Remove todo' do
+    fill_in 'Name', with: 'Buy Banana'
+    click_button 'Create Todo'
+    expect(page).to have_content("Buy Banana")
+    expect(Todo.count).to eq(1)
+    expect(Todo.last.name).to eq("Buy Banana")
+    click_button 'Delete'
+    expect(Todo.count).to eq(0)
+  end
+
+  scenario 'Move todo to complete' do
+    fill_in 'Name', with: 'Buy Banana'
+    click_button 'Create Todo'
+    expect(page).to have_content("Buy Banana")
+    expect(Todo.count).to eq(1)
+    expect(Todo.last.name).to eq("Buy Banana")
+    click_button 'Mark as complete'
+    click_link 'Complete'
+    expect(page).to have_content("Buy Banana")
+    expect(Todo.last.status).to eq('complete')
+  end  
 end
