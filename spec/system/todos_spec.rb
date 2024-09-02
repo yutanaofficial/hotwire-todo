7189bc2b59c84210d8f9dd3277d046639edc70cf
Ruuf <ruuf@odds.team>
2024-09-02 08:45:01 +0700

    Revise test

--- spec/system/todos_spec.rb
+++ spec/system/todos_spec.rb
@@ -2,22 +2,18 @@ require 'rails_helper'
 
 RSpec.describe "Todos", type: :system do
   before do
+    # puts "Before"
+    visit todos_path
     driven_by(:rack_test)
   end
 
   scenario 'Invalid todo name' do
-    visit todos_path
-    expect(page).to have_content("Incomplete")
-    expect(page).to have_content("Complete")
     click_button 'Create Todo'
     expect(page).to have_content("Name can't be blank")
     expect(Todo.count).to eq(0)
   end
 
   scenario 'Valid todo name' do
-    visit todos_path
-    expect(page).to have_content("Incomplete")
-    expect(page).to have_content("Complete")
     fill_in 'Name', with: 'Buy Banana'
     click_button 'Create Todo'
     expect(page).to have_content("Buy Banana")
