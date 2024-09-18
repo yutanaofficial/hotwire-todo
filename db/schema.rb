ce024e103832d94b6187613440c697c79db85d16
Ruuf <ruuf@odds.team>
2024-09-18 10:57:14 +0700

    Change dev database to sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_19_035123) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_19_035123) do
   create_table "todos", force: :cascade do |t|
     t.string "name"
     t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
